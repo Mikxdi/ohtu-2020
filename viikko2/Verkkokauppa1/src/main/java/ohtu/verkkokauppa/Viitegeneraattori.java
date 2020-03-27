0f85afeddeba47de488f0c9850037c070a83e7a5
Mikxdi <mikko.lehtinen1996@gmail.com>
2020-03-27 10:55:13 +0200

    viikko2

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,18 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Viitegeneraattori implements ViitegeneraattoriInterface {
+
+
+    private int seuraava;
+    
+    private Viitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    public int uusi(){
+        return seuraava++;
+    }
+}
